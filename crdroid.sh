c12af7f06786670e756414ed6bdd0f95605a0014
Mishun <misha.papsuev8@gmail.com>
2026-06-03 11:38:49 +0300

    Update crdroid.sh

--- crdroid.sh
+++ crdroid.sh
@@ -3,12 +3,13 @@ rm -rf .repo/local_manifests
 # Repo init CrDroid 15
 repo init -u https://github.com/crdroidandroid/android -b 15.0 --depth=1 --git-lfs
 
+# Удаляем старый clang
 rm -rf prebuilts/clang/host/linux-x86
 
 # Sync
 /opt/crave/resync.sh
 
-# Clear
+# Очистка старых исходников устройства
 rm -rf device/zte/akershus
 rm -rf vendor/zte/akershus
 rm -rf kernel/zte/sdm845
@@ -22,6 +23,7 @@ git clone https://github.com/rtx4d/proprietary_vendor_zte_akershus -b lineage-22
 # Kernel
 git clone https://github.com/rtx4d/android_kernel_zte_sdm845 -b lineage-22.2 kernel/zte/sdm845
 
-# Build
+# Сборка
 . build/envsetup.sh
-lunch crdroid_akershus-user && mka bacon
+lunch crdroid_akershus-ap4a-user
+mka bacon
